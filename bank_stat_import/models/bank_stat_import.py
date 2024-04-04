48447a66774fd5886e9543beb0b574c0ecf8f97b
wiwiw-wiwiw <wiwiw.ru@gmail.com>
2024-04-05 00:12:49 +0400

    какой то рабочий вариант, извлекаем дату сумму, тип операции, коментарий и создаем записи в account.payment

--- bank_stat_import/models/bank_stat_import.py
+++ bank_stat_import/models/bank_stat_import.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 from lxml import etree
 import xml.etree.ElementTree as ET
 import base64
-
+from datetime import datetime
 
 
 class BankStatImport(models.Model):
@@ -12,6 +12,7 @@ class BankStatImport(models.Model):
     name = fields.Char(string='Наименование')
     date = fields.Date(string='Дата')
     amount = fields.Float(string='Сумма')
+
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
@@ -29,27 +30,66 @@ class AccountPayment(models.Model):
             xml_etree = etree.fromstring(content_data)
 
             extractList_elems = xml_etree.find("{*}extractList")
-            # print(extractList_elems)
-
+            i=0
+            print("Step 2 extractList_elems", extractList_elems)
             for turn in extractList_elems.findall("{*}turns"):
+                # print("step 3 turn ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||", turn)
+                i=i+1
+                print(i)
                 crAmount = turn.find("{*}crAmount").text
+                dbAmount = turn.find("{*}dbAmount").text
                 naznText = turn.find("{*}naznText").text
-                # print("crAmount", crAmount, "naznText", naznText)
+                docDate = turn.find("{*}docDate").text
 
+                date_object = datetime.strptime(docDate, "%Y-%m-%dT%H:%M:%S%z")
+                date_only = date_object.date()
+
+                docN = turn.find("{*}docN").text
+                corrName = turn.find("{*}corrName").text
+                corrAccount = turn.find("{*}corrAccount").text
+                corrBankCode = turn.find("{*}corrBankCode").text
+                # corrBankName = turn.find("{*}corrBankName").text
+
+                # Извлечение значения TurnType
+                turn_type_value = turn.find(".//addParams/entry[key='TurnType']/value")
+                turn_type = turn_type_value.text if turn_type_value is not None else None
+            
+
+                        
+                # формирование payment_data
+                
+                if turn_type == "DEBET":
+                    payment_type = "outbound"
+                    amount = dbAmount
+                elif turn_type == "CREDIT":
+                    payment_type = "inbound"
+                    amount = crAmount
+                
                 payment_data = {
-                    'amount': crAmount, 
-                    'ref': naznText
-                    }
+                    # 'turn_type': turn_type, # test
+                    'amount': amount, # сумма 
+                    'payment_type': payment_type,  # Send or Receive
+                    # 'crAmount': crAmount, # test
+                    # 'dbAmount': dbAmount, # test
+                    'ref': naznText,  # комментарий - назначение платежа
+                    'date': date_only,  # дата
+                    
+                    
+
+
+                    
+
+                }
+                # создание платежа
                 new_payment = self.env['account.payment'].create(payment_data)
                 print("PAYMENT DATA", payment_data)
-            
-               
+
         return {
             # 'type': 'ir.actions.act_window',
             # 'name': 'Vendor Payments',
             # 'view_mode': 'tree,form',
             # 'res_model': 'account.payment',
             # 'target': 'current',
-            
+
             # 'domain': ["&", ("partner_type", "=", "supplier"), ("is_internal_transfer", "=", False)],
         }
